4b81693a9783a6642de3b20f3e37b0d19a8339a3
dmitrykurmanov <kurmanov.work@gmail.com>
2026-04-28 16:05:57 +0400

    changed PW start commang to prod

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
